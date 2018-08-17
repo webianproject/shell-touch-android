e47bdcbdb84476b0f17d038395354c69bbaa8cb8
Ben Francis <ben@tola.me.uk>
2018-08-17 10:48:12 +0100

    Port to GeckoView

--- app/src/main/java/org/webian/shelltouch/HomeActivity.java
+++ app/src/main/java/org/webian/shelltouch/HomeActivity.java
@@ -3,8 +3,10 @@ package org.webian.shelltouch;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoRuntimeSettings;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,7 +18,9 @@ public class HomeActivity extends Activity {
 
     private ShellDatabase database;
     private View mContentView;
-    private WebView webview;
+    private GeckoView geckoview;
+    private GeckoSession session;
+    private static GeckoRuntime runtime;
     private static final String HOME_PAGE = "https://duckduckgo.com";
 
     @Override
@@ -27,12 +31,44 @@ public class HomeActivity extends Activity {
         ArrayList apps = database.getApps();
         System.out.println("Apps: " + apps);
         mContentView = findViewById(R.id.home_content);
-        webview = findViewById(R.id.webview);
-        webview.setWebViewClient(new WebViewClient());
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setDomStorageEnabled(true);
-        webview.setInitialScale(100);
-        webview.loadUrl(HOME_PAGE);
+
+        // Find the GeckoView in our layout
+        geckoview = (GeckoView) findViewById(R.id.geckoview);
+
+        if (runtime == null) {
+            final GeckoRuntimeSettings.Builder runtimeSettingsBuilder =
+                    new GeckoRuntimeSettings.Builder();
+
+            if (BuildConfig.DEBUG) {
+                // In debug builds, we want to load JavaScript resources fresh with
+                // each build.
+                runtimeSettingsBuilder.arguments(new String[] { "-purgecaches" });
+            }
+
+            final Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                runtimeSettingsBuilder.extras(extras);
+            }
+            runtimeSettingsBuilder
+                    .useContentProcessHint(false)
+                    .remoteDebuggingEnabled(true)
+                    .nativeCrashReportingEnabled(true)
+                    .javaCrashReportingEnabled(true)
+                    .crashReportingJobId(1024)
+                    .consoleOutput(true)
+                    .displayDensityOverride(1)
+                    .trackingProtectionCategories(GeckoSession.TrackingProtectionDelegate.CATEGORY_ALL);
+
+            runtime = GeckoRuntime.create(this, runtimeSettingsBuilder.build());
+        }
+
+        // Attach the GeckoView to a new GeckoSession
+        session = new GeckoSession();
+        session.open(runtime);
+        geckoview.setSession(session);
+
+        // Load a URL
+        session.loadUri(HOME_PAGE);
     }
 
     /**
@@ -41,7 +77,7 @@ public class HomeActivity extends Activity {
      * @param view
      */
     public void home(View view) {
-        webview.loadUrl(HOME_PAGE);
+        session.loadUri(HOME_PAGE);
     }
 
     /**
@@ -50,7 +86,7 @@ public class HomeActivity extends Activity {
      * @param view
      */
     public void back(View view) {
-        webview.goBack();
+        session.goBack();
     }
 
     /**
@@ -59,7 +95,7 @@ public class HomeActivity extends Activity {
      * @param view
      */
     public void reload(View view) {
-        webview.reload();
+        session.reload();
     }
 
     @Override
